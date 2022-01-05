403752b2fee46a7cfd7aed2990f3b5ef3907cd29
nam <than2557@gmail.com>
2022-01-05 13:08:56 +0700

    set full screen

--- getstart.js
+++ getstart.js
@@ -8,7 +8,7 @@ const fsp = require('fs').promises;
     try {
 
         await driver.get("https://172.23.31.1/php/login.php");
-        await driver.manage().window().setRect({ width: 1980, height: 1500 });
+        await driver.manage().window().setRect({ fullscreen: true });
         await driver.findElement(By.id("details-button")).click();
         await driver.findElement(By.id("proceed-link")).click();
         await driver.findElement(By.id("user")).sendKeys("nocservice");
@@ -19,7 +19,7 @@ const fsp = require('fs').promises;
         await driver.findElement(By.xpath("/html/body/div[3]/div/div[2]/div[2]/div/div/div[1]/div[2]/div[1]/div/div/div/div/div/div/ul/div/li[4]/ul/li[3]/div/a/span")).click();
         await driver.findElement(By.xpath("/html/body/div[3]/div/div[1]/div/table/tbody/tr/td[1]/table/tbody/tr/td[6]/div/img")).click();
 
-       await driver.findElements(BY.variable("ABP12")).click();
+       await driver.findElements(By.variable("ABP12")).click();
         // await driver.findElement(By.xpath("/html/body/div[15]/div/div[5]")).click();   
      
    
