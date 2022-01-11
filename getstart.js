d4f200aa868455831f8f3e3b0c787a4242519eb5
nam <than2557@gmail.com>
2022-01-11 17:23:58 +0700

    success capture picture

--- getstart.js
+++ getstart.js
@@ -32,26 +32,26 @@ const { createCanvas, loadImage,toDataURL,html2canvas } = require('html2canvas')
         // await driver.findElement(By.id("ext-gen218")).click();
         // await driver.wait(until.elementLocated(By.id("chart_container_parent")),70000);
    
+      
+      
         await driver.wait(until.elementsLocated(By.id("ext-gen218")),70000);
-        await driver.wait(until.elementlocation(By.xpath("/html/body/div[2]/div/div[2]/div/div/div/div[2]/div/div/div/div[2]/div[1]/div/svg/g[5]/g[1]/rect[11]")),70000);
-        await driver.findElement(By.id("ext-gen218")).isEnabled();
-        // await driver.findElement(By.id("ext-gen218")).click();
+        // await driver.wait(until.elementLocated(By.xpath("/html/body/div[2]/div/div[2]/div/div/div/div[2]/div/div/div/div[2]/div[1]/div/svg/g[6]/g/g/g[1]/text/tspan")),70000);
+       await driver.wait(until.elementLocated(By.xpath("/html/body/div[2]/div/div[2]/div/div/div/div[2]/div/div/div/div[2]/div[1]/div/svg/g[6]/g/g/g[1]/text/tspan")),70000);
+       
+
+    } finally {
         
         await  driver.takeScreenshot().then(
             function(image) {
                 require('fs').writeFileSync('captured_image_3.png', image, 'base64');
             }
         );
-      
-        // var chart = await driver.findElement(By.id("highcharts-6"));
-        // await driver.wait(until.elementLocated(By.xpath("/html/body/div[2]/div/div[2]/div/div/div/div[2]/div/div/div/div[1]/div/table/tbody/tr/td[1]/table/tbody/tr/td[13]/table/tbody/tr[2]/td[2]/em/button")), 70000);
-    
-
-    } finally {
-        // await driver.quit();
+     
     }   
 })
 
 ();
 
 
+
+        
