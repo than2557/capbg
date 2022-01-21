8dfc9e0ad838b1367b6a252f83879ebe87ef5eba
nam <than2557@gmail.com>
2022-01-21 10:40:38 +0700

    sharp install

--- getstart.js
+++ getstart.js
@@ -29,31 +29,32 @@ const { jquery,$} = require('jquery');
         await driver.findElement(By.id("passwd")).sendKeys("NOCpwd2020+");
         await driver.findElement(By.id("submit")).click();
         await driver.wait(until.elementLocated(By.id("monitor_img")), 70000);
-        await driver.findElement(By.xpath("/html/body/div[1]/div/div/table/tbody/tr[1]/td/table/tbody/tr/td[2]/table/tbody/tr/td[4]")).click();
-        await driver.findElement(By.xpath("/html/body/div[3]/div/div[2]/div[2]/div/div/div[1]/div[2]/div[1]/div/div/div/div/div/div/ul/div/li[4]/ul/li[3]/div/a/span")).click();
-
-        await driver.get("https://172.23.31.1/?#monitor::ABP3::monitor/app-scope/threat-monitor");
+        //await driver.findElement(By.xpath("/html/body/div[1]/div/div/table/tbody/tr[1]/td/table/tbody/tr/td[2]/table/tbody/tr/td[4]")).click();
+        //await driver.findElement(By.xpath("/html/body/div[3]/div/div[2]/div[2]/div/div/div[1]/div[2]/div[1]/div/div/div/div/div/div/ul/div/li[4]/ul/li[3]/div/a/span")).click();
+        await driver.sleep(5000);
+    //    await driver.findElement(By.id('monitor')).click();
+    await driver.findElement(By.xpath("/html/body/div[1]/div/div/table/tbody/tr[1]/td/table/tbody/tr/td[2]/table/tbody/tr/td[4]")).click();
         
         // await driver.wait(until.elementsLocated(By.id("highcharts-3")),100000).isEnabled();
-     
+     await driver.get("https://172.23.31.1/?#monitor::ABP3::monitor/app-scope/threat-monitor");
         // await driver.findElement(By.id("ext-gen218")).click();
         // await driver.wait(until.elementLocated(By.id("chart_container_parent")),70000);
    
        
       
-        await driver.wait(until.elementsLocated(By.id("ext-gen218")),70000);
-        await driver.sleep(5000);
+        // await driver.wait(until.elementsLocated(By.id("ext-gen218")),70000);
+        // await driver.sleep(5000);
      
 
     } finally {
         
         // screen.screenchart(); fix screen
-    const findEl = await driver.findElement(By.id("chart_container"));
-        await  driver.takeScreenshot(findEl).then(
-            function(image) {
-                require('fs').writeFileSync('chart.png', image, 'base64');
-            }
-        );
+    // const findEl = await driver.findElement(By.id("chart_container"));
+    //     await  driver.takeScreenshot(findEl).then(
+    //         function(image) {
+    //             require('fs').writeFileSync('chart.png', image, 'base64');
+    //         }
+    //     );
    
 
     }   
